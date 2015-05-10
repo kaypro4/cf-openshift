0e3c86082a281267f7548a66ddf2d02cffb59336
Matt Pope <matt@parallelpublicworks.com>
2015-05-10 14:57:42 -0700

    Removed container from all panel pages

--- sites/all/themes/brighton/layouts/basic_no_right_col/basic-no-right-col.tpl.php
+++ sites/all/themes/brighton/layouts/basic_no_right_col/basic-no-right-col.tpl.php
@@ -6,23 +6,21 @@
 
 
 
-    <div class="container">
-    
-      <div class="row">
-	  	   <div class="col-md-10 col-md-push-2">
-	   	   <?php print $content['title']; ?>
-	      </div>
-	   </div>
-      <div class="row">
 
-        <div class="col-md-2 left content-nav">
-          <?php print $content['nav']; ?>
-        </div>
+<div class="row">
+   <div class="col-md-10 col-md-push-2">
+	   <?php print $content['title']; ?>
+  </div>
+</div>
+<div class="row">
 
-        <div class="col-md-10 middle content-col">
-          <?php print $content['content']; ?>
-        </div>
-        
+  <div class="col-md-2 left content-nav">
+    <?php print $content['nav']; ?>
+  </div>
 
-      </div>
-    </div>
+  <div class="col-md-10 middle content-col">
+    <?php print $content['content']; ?>
+  </div>
+
+
+</div>
--- sites/all/themes/brighton/layouts/basic_right_col/basic-right-col.tpl.php
+++ sites/all/themes/brighton/layouts/basic_right_col/basic-right-col.tpl.php
@@ -6,26 +6,25 @@
 
 
 
-    <div class="container">
-    
-      <div class="row">
-	  	   <div class="col-md-10 col-md-push-2">
-	   	   <?php print $content['title']; ?>
-	      </div>
-	   </div>
-      <div class="row">
 
-        <div class="col-md-2 left content-nav">
-          <?php print $content['nav']; ?>
-        </div>
 
-        <div class="col-md-7 middle content-col">
-          <?php print $content['content']; ?>
-        </div>
-        
-        <div class="col-md-3 right right-col">
-          <?php print $content['right']; ?>
-        </div>
+<div class="row">
+   <div class="col-md-10 col-md-push-2">
+	   <?php print $content['title']; ?>
+  </div>
+</div>
+<div class="row">
 
-      </div>
-    </div>
+  <div class="col-md-2 left content-nav">
+    <?php print $content['nav']; ?>
+  </div>
+
+  <div class="col-md-7 middle content-col">
+    <?php print $content['content']; ?>
+  </div>
+
+  <div class="col-md-3 right right-col">
+    <?php print $content['right']; ?>
+  </div>
+
+</div>
