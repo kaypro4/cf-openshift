5735e360dbb4f42002d8fefadb71d7e07a0b070f
Matt Pope <matt@parallelpublicworks.com>
2015-05-10 09:15:53 -0700

    Removed extra container

--- sites/all/themes/brighton/layouts/basic_no_nav/basic-no-nav.tpl.php
+++ sites/all/themes/brighton/layouts/basic_no_nav/basic-no-nav.tpl.php
@@ -6,22 +6,19 @@
 
 
 
-    <div class="container">
-    
-      <div class="row">
-	  	   <div class="col-md-12">
-	   	   <?php print $content['title']; ?>
-	      </div>
-	   </div>
-      <div class="row">
+<div class="row">
+   <div class="col-md-12">
+	   <?php print $content['title']; ?>
+  </div>
+</div>
+<div class="row">
 
-        <div class="col-md-9 middle content-col">
-          <?php print $content['content']; ?>
-        </div>
-        
-        <div class="col-md-3 right right-col">
-          <?php print $content['right']; ?>
-        </div>
+  <div class="col-md-9 middle content-col">
+    <?php print $content['content']; ?>
+  </div>
 
-      </div>
-    </div>
+  <div class="col-md-3 right right-col">
+    <?php print $content['right']; ?>
+  </div>
+
+</div>
